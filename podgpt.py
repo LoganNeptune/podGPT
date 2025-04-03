95cbf9f48161e67e6b81e0970ba688206264a2dc
Logan Neptune <102203064+LoganNeptune@users.noreply.github.com>
2025-04-03 03:32:55 -0400

    Attention

--- podgpt.py
+++ podgpt.py
@@ -7,7 +7,7 @@ batch_size = 32 # how many indepenent sequences will we process in parellel
 block_size = 8 # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
-learning_rate = le-2
+learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
@@ -16,7 +16,7 @@ n_embd = 32
 torch.manual_seed(1337)
 
 # wget https://github.com/karpathy/char-rnn/tree/master/data/tinyshakespeare
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('pod.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 #unique characters that occur in this text
@@ -38,9 +38,9 @@ val_data = data[n:]
 def get_batch(split):
   # generate a small batch of inputs x and targets y
   data = train_data if split == 'train' else val_data
-  ix = torch.randint(len(data) -block_size, (batch_size,))
-  x = torch.stack([data[i:i*block_size] for i in ix])
-  y = torch.stack([data[i+1: i+block_size*1] for i in ix])
+  ix = torch.randint(len(data) - block_size, (batch_size,))
+  x = torch.stack([data[i:i+block_size] for i in ix])
+  y = torch.stack([data[i+1:i+block_size+1] for i in ix])
   x, y = x.to(device), y.to(device)
   return x, y
 
@@ -58,46 +58,78 @@ def estimate_loss():
   model.train()
   return out
       
+class Head(nn.Module):
+    """ one head of self-attention"""
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x) # (B, T, 16)
+        q = self.query(x) # (B, T, 16)
+        #compute attention scores ("affinities")
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, 16) @ (B, T, 16) ---> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        # perform aggregation on the values
+        v = self.value(x) # (B,T,C)
+        out = wei @ v # (B,T,T)(B,T,C)--->(B,T,C)
+        return out
+
 # super simple bigram model
 class BigramLanguageModule(nn.Module):
 
-    def __init__(self, vocab_size):
+  def __init__(self):
       super().__init__()
       # each token directly reads off the logits for the next token from a lookup table
       self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
-      self.
+      self.position_embedding_table = nn.Embedding(block_size, n_embd)
+      self.sa_head = Head(n_embd)
+      self.lm_head = nn.Linear(n_embd, vocab_size)
 
-    def forward(self, idx, target=None):
-   
+  def forward(self, idx, targets=None):
+    B, T = idx.shape
+
      # idx and targets are both (B, T) tonsor of integers
-    tok_emb = self.token_embedding_table(idx) # (B, T, C)
+    tok_emb = self.token_embedding_table(idx) # (B,T,C)
+    pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+    x = tok_emb + pos_emb # (B,T,C)
+    x = self.sa_head(x) # Apply one head of self-attention. (B, T, C)
+    logits = self.lm_head(x) # (B,T, vocab_size)
 
     if targets is None:
-      loss = None
+        loss = None
     else:
-      B, T, C = logits.shape
-      logits = logits.view(B*T, C)
-      targets = targets.view(B*T)
-      loss = F.cross_entropy(logits, targets)
+        B, T, C = logits.shape
+        logits = logits.view(B*T, C)
+        targets = targets.view(B*T)
+        loss = F.cross_entropy(logits, targets)
+        
+    return logits,loss
 
-  return logits, loss
-
-def generate(self, idx, max_new_tokens):
+  def generate(self, idx, max_new_tokens):
    # idx is {B, T} array of indices in the current context
-   for _ in range(max_new_tokens):
+      for _ in range(max_new_tokens):
+      # crop idx to the last block_size_tokens
+        idx_cond = idx[:, -block_size:]
       # get the predictions
-      logits, loss = self(idx)
+        logits, loss = self(idx_cond)
       # focus only on the last time step
-      logits = logits[:, -1 :] # becomes (B, C)
+        logits = logits[:, -1, :] # becomes (B, C)
       # apply softmax to get probabilities
-      probs = F.softmax(logits, dim=-1) # (B, C)
+        probs = F.softmax(logits, dim=-1) # (B, C)
       # sample from the distribution
-      idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+        idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
       # append sampled index to the running sequence
-      idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
       return idx
    
-   model = BigramLanguageMode(vocab_size)
+model = BigramLanguageModule()
 m = model.to(device)
 
 # create a PyTorch optimizer
@@ -107,21 +139,21 @@ for iter in range(max_iters):
 
     # every once in a while evaluate the loss on the train and val sets
     if iter % eval_interval == 0:
-      losses = estimate_loss[]
+      losses = estimate_loss()
       print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
     # sample a batch of data
-    xb, yb = get_batch['train']
+    xb, yb = get_batch('train')
 
     # evaluate the loss
-    logits, loss = model[xb, yb]
-    optimizer.zero_grad[set_to_none=True]
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
 # generate from the model
-context = torch.zeros((:, 1) dtype=torch.long, device=device)
-print(decode(m.generate[context, max_new_tokens=500][0].tolist()))
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
 
 
 block_size = 8
@@ -221,7 +253,7 @@ for steps in range(100):
 
   # sample a batch of data
   xb, yb = get_batch('train')
-TV
+  
   # evaluate the loss
   logits, loss = m(xb, yb)
   optimizer.zero_grad(set_to_none=True)
@@ -244,19 +276,13 @@ xbow = torch.zeros((B,T,C))
 for b in range(B):
     for t in range(T):
         xprev = x[b,:t+1] # (t,C)
-        torch.allclose(xbow[b,t] = torch.mean(xprev, 0))
+        xbow[b,t] = torch.mean(xprev, 0)
 
 # version 2
 wei = torch.tril(torch.ones(T, T))
 wei = wei / wei.sum(1, keepdim=True)
 xbow2 = wei @ x # (B, T, T,) @ (B, T, C) ----> (B, T, C)
 torch.allclose(xbow, xbow2)
-
-tril
-
-wei = torch.zeros((T,T))
-wei = wei.masked_fill(tril == 0, float('-inf'))
-wei = F.softmax
 
 # version 3
 tril = torch.tril(torch.ones(T, T))
@@ -269,8 +295,8 @@ torch.allclose(xbow, xbow3)
 torch.tril(torch.ones(3, 3))
 
 torch.manual_seed(42)
-a = torch.trill(torch.ones(3, 3))
-a = torch / torch.sum(a, 1, keepdim=True)
+a = torch.tril(torch.ones(3, 3))
+a = a / torch.sum(a, 1, keepdim=True)
 b = torch.randint(0,10,(3,2)).float()
 c = a @ b
 print('a=') #Muliply row...
