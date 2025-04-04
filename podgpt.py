b5634ce507e7f7f4375458a12cde6295d27cb1ba
Logan Neptune <102203064+LoganNeptune@users.noreply.github.com>
2025-04-04 09:56:37 +0000

    attention

--- podgpt.py
+++ podgpt.py
@@ -81,6 +81,17 @@ class Head(nn.Module):
         out = wei @ v # (B,T,T)(B,T,C)--->(B,T,C)
         return out
 
+class MultiHeadAttention(nn.Module):
+   """multiple heads of self attention in parralel"""
+
+   def __init__(self, num_heads, head_size):
+      super().__init__()
+      self.heads = nn.ModuleList((Head(head_size) for _ in range(num_heads)))
+      
+      def forward(self, x):
+         return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
 # super simple bigram model
 class BigramLanguageModule(nn.Module):
 
